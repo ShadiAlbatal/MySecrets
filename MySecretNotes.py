aee557e38badb4598b71ad324497631ea951d337
shadialbatal <shadialbatal1993@hotmail.com>
2019-07-20 00:58:30 +0200

    bug fixes

--- MySecretNotes.py
+++ MySecretNotes.py
@@ -1,3 +1,4 @@
+
 import datetime
 import os
 
@@ -10,45 +11,61 @@ def search(username, password):
                 return True
     return False
 
+def verify(username):
+    for line in open("userlist.txt","r").readlines():
+            login_info = line.split()
+            if username == login_info[0]:
+                return True
+    return False
+
 
 with open('userlist.txt', 'a+') as my_file:
-   Greet = input("Are you a member of us? type:  \ny to login \nn to continue \n ")
-   if Greet == 'y':
-       print("Welcome Dear")
-   else:
-        membership = input("Would you like to become a member? type: \ny to create new \npress any key else to exit\n")
-        if membership == 'y':
-           create_user = input("Please type your new-username:  " "\n")
-           create_pass = input("Please type a password:  " "\n")
-           my_file.write(' '.join([create_user, create_pass])+ "\n")
-           create_secret= create_user + create_pass + ".txt"
-           open(create_secret,"a+")
-           print("Your username has nw been created.\nlogin please. ")
+    while True:
+        Greet = input("warm welcome to your app my secrets, type:  \ny to login \nc to create \n ")
+        if Greet == 'y':
+            print("Welcomeback Dear")
+            break
+        elif Greet == 'c':
+            while True:
+                create_user = input("Please type your new-username:  " "\n")
+                create_pass = input("Please select a password:  " "\n")
+                if verify(create_user):
+                    print("username is already taken, try again")
+                else:
+                    my_file.write(' '.join([create_user, create_pass])+ "\n")
+                    create_secret= create_user + create_pass + ".txt"
+                    open(create_secret,"a+")
+                    print("Your username has been created.\nyou will be directed to the start page. ")
+                    break
         else:
-           print("thank you for visiting, take care")
-           quit()
+            print(" did not understand, try again please ")
+
 
 with open('userlist.txt', 'a+') as my_file:
-   username = input("Type in your Username:  " "\n")
-   password = input("Type in your Password:  " "\n")
    while True:
-       if search(username, password):
-           print ("your ntes: \n")
-           open_nte_RBI= username + password + ".txt"
-           LHMDL_read_secrets= open(open_nte_RBI,"r").read()
-           print (LHMDL_read_secrets)
-           choice = input("Do you want is in your mind? \nn to add new secret \nd to prune all your secrets (might work only on linux) \nq to quit\n")
-           if choice == "n":
-                new_nte_LLH = input("Tell me please\n")
-                BSMLH_open_= open(open_nte_RBI, "a+")
-                HKBR_add_new = new_nte_LLH + "\n" + str(datetime.datetime.now())
-                BSMLH_open_.write(HKBR_add_new + "\n\n")
-                BSMLH_open_.close()
+        username = input("Type in your Username:  " "\n")
+        password = input("Type in your Password:  " "\n")
+        if search(username, password):
+            while True:
+                print ("your notes: \n")
+                open_nte_RBI= username + password + ".txt"
+                LHMDL_read_secrets= open(open_nte_RBI,"a+").read()
+                print (LHMDL_read_secrets)
+                choice = input("Do you want is in your mind? \nn to add new secret \nd to prune all your secrets (might work only on linux) \nq to quit\n")
+                if choice == "n":
+                    new_nte_LLH = input("Tell me please\n")
+                    BSMLH_open_= open(open_nte_RBI, "a+")
+                    HKBR_add_new = new_nte_LLH + "\n" + str(datetime.datetime.now())
+                    BSMLH_open_.write(HKBR_add_new + "\n\n")
+                    BSMLH_open_.close()
             ## the d option might work only on linux
-           elif choice == "d":
-                cwd = os.getcwd()
-                os.remove(cwd + '/' + open_nte_RBI)
-           elif choice == "q":
-                quit()
-           else:
-                print("didnt understand")
+                elif choice == "d":
+                    cwd = os.getcwd()
+                    os.remove(cwd + '/' + open_nte_RBI)
+                    open(open_nte_RBI, "w").close()
+                elif choice == "q":
+                    quit()
+                else:
+                    print("didnt understand")
+        else:
+            print("try again")
